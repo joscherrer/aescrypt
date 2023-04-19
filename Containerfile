25da0f2429b651336c3d7e811e156e444d2bee95
Jonathan Scherrer <jonathan@scherrer.ovh>
2023-04-19 10:17:07 +0200

    init

---
+++ Containerfile
@@ -0,0 +1,73 @@
+FROM registry.access.redhat.com/ubi9/ubi-minimal:9.1 AS vault-builder
+
+ARG VAULT_VERSION=1.13.1
+ARG ARCH=amd64
+
+ARG VER_CA_CERTIFICATES=2022.*
+ARG VER_OPENSSL=1:3.0.1*
+ARG VER_LIBCAP=2.48*
+ARG VER_TZDATA=2023c*
+ARG VER_UNZIP=6.0*
+ARG VER_SHADOW_UTILS=2:4.9*
+ARG VER_UTIL_LINUX=2.37*
+ARG VAULT_RELEASE_URL=https://releases.hashicorp.com/vault
+
+RUN set -eux; \
+    microdnf install -y \
+        ca-certificates-${VER_CA_CERTIFICATES} \
+        shadow-utils-${VER_SHADOW_UTILS} \
+        util-linux-${VER_UTIL_LINUX} \
+        openssl-${VER_OPENSSL} \
+        libcap-${VER_LIBCAP} \
+        tzdata-${VER_TZDATA} \
+        unzip-${VER_UNZIP} \
+        && \
+    mkdir -p /tmp/build && \
+    curl -L ${VAULT_RELEASE_URL}/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_${ARCH}.zip -o /tmp/vault_${VAULT_VERSION}_linux_${ARCH}.zip && \
+    unzip -d /tmp/build /tmp/vault_${VAULT_VERSION}_linux_${ARCH}.zip && \
+    setcap cap_ipc_lock=+ep /tmp/build/vault && \
+    microdnf clean all
+
+FROM registry.access.redhat.com/ubi9/openjdk-17:1.14 AS aescrypt-builder
+USER root
+COPY . /tmp/build/
+WORKDIR /tmp/build
+RUN mvn clean compile package
+
+
+FROM registry.access.redhat.com/ubi9/ubi-minimal:9.1
+
+ARG VER_CA_CERTIFICATES=2022.*
+ARG VER_OPENSSL=1:3.0.1*
+ARG VER_LIBCAP=2.48*
+ARG VER_TZDATA=2023c*
+ARG VER_UNZIP=6.0*
+ARG VER_SHADOW_UTILS=2:4.9*
+ARG VER_UTIL_LINUX=2.37*
+ARG VER_OPENJDK_17=17.0.*
+
+COPY --from=vault-builder /tmp/build/vault /usr/local/bin/vault
+COPY --from=aescrypt-builder /tmp/build/target/aescrypt-*-dependencies.jar /opt/aescrypt/aescrypt.jar
+COPY --from=aescrypt-builder /tmp/build/aescrypt /usr/local/bin/aescrypt
+
+RUN microdnf --setopt=install_weak_deps=0 --setopt=tsflags=nodocs -y install \
+        ca-certificates-${VER_CA_CERTIFICATES} \
+        shadow-utils-${VER_SHADOW_UTILS} \
+        openssl-${VER_OPENSSL} \
+        java-17-openjdk-headless-1:${VER_OPENJDK_17} \
+        && \
+    groupadd --gid 1001 vault && \
+    adduser --uid 1001 -g vault vault && \
+    microdnf clean all && \
+    rm -rf /var/lib/rpm && \
+    rm -rf /var/cache/yum && \
+    mkdir -p /vault/logs /vault/file /vault/config && \
+    chown -R vault:vault /vault && \
+    chmod +x /usr/local/bin/aescrypt
+
+USER vault
+WORKDIR /vault
+
+EXPOSE 8200
+# ENTRYPOINT ["vault"]
+# CMD ["agent"]
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,118 @@
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.security.KeyStore
+import java.security.KeyStore.SecretKeyEntry
+import java.security.SecureRandom
+import java.security.UnrecoverableKeyException
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import kotlin.system.exitProcess
+
+
+val aesKey = System.getenv("AES_KEY") ?: null;
+val aesKeyPassword: CharArray = (System.getenv("AES_KEY_PASSWORD") ?: "changeit").toCharArray();
+val keystorePath: String = System.getenv("KEYSTORE_PATH") ?: "keystore.p12";
+val keystorePassword: CharArray = (System.getenv("KEYSTORE_PASSWORD") ?: "changeit").toCharArray();
+val keystoreType: String = System.getenv("KEYSTORE_TYPE") ?: "PKCS12";
+val aesAlgorithm: String = System.getenv("AES_ALGORITHM") ?: "CBC/PKCS5Padding";
+val debug: Boolean = (System.getenv("DEBUG") ?: "false").toBoolean()
+
+fun ByteArray.toHexString() = joinToString("") { "%02x".format(it) }
+
+fun String.decodeHex(): ByteArray {
+    check(length % 2 == 0) { "Must have an even length" }
+    return chunked(2).map { it.toInt(16).toByte() }.toByteArray()
+}
+
+fun eprintln(message: String) {
+    System.err.println(message)
+}
+
+fun randomSecretKey(): SecretKey {
+    val generator = KeyGenerator.getInstance("AES")
+    generator.init(256, SecureRandom.getInstanceStrong())
+    return generator.generateKey()
+}
+
+fun genSecretKeyEntry(key: ByteArray? = null, algo: String = "AES"): SecretKeyEntry {
+    val secretKey: SecretKey = if (key != null) SecretKeySpec(key, algo) else randomSecretKey()
+    return SecretKeyEntry(secretKey)
+}
+
+fun genIvParameterSpec(algorithm: String): IvParameterSpec {
+    val iv = ByteArray(Cipher.getInstance(algorithm).blockSize)
+    SecureRandom.getInstanceStrong().nextBytes(iv);
+    return IvParameterSpec(iv)
+}
+
+fun encrypt(algorithm: String, rawData: String, key: SecretKey): String {
+    val iv: IvParameterSpec = genIvParameterSpec(algorithm)
+    val cipher = Cipher.getInstance(algorithm);
+    cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+    val cipherText = cipher.doFinal(rawData.toByteArray());
+    return iv.iv.toHexString() + cipherText.toHexString();
+}
+
+fun decrypt(algorithm: String, ciphertext: String, key: SecretKey): String {
+    val cipher = Cipher.getInstance(algorithm)
+    val ivRaw = ciphertext.slice(0 until ciphertext.length/2).decodeHex()
+    val iv = IvParameterSpec(ivRaw)
+    val cipherData = ciphertext.slice(ciphertext.length/2 until ciphertext.length).decodeHex()
+    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+    val data = cipher.doFinal(cipherData)
+
+    return String(data)
+}
+
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        eprintln("No data to encrypt")
+        exitProcess(1)
+    }
+
+    val algorithm = "AES/$aesAlgorithm" // Only AES supported
+    val rawData: String = args[0] // Data to encrypt
+    val keystore: KeyStore = KeyStore.getInstance(keystoreType)
+    val keystoreOutputStream = FileOutputStream(keystorePath)
+    val keystoreInputStream = FileInputStream(keystorePath)
+
+    try {
+        keystore.load(keystoreInputStream, keystorePassword)
+    } catch (e: IOException) {
+        if (e.cause is UnrecoverableKeyException) {
+            eprintln("Couldn't load keystore from disk, wrong password")
+            exitProcess(1)
+        }
+        // Create an empty keystore
+        keystore.load(null, keystorePassword)
+    } catch (e: Exception) {
+        eprintln("Couldn't load keystore from disk.")
+        exitProcess(1)
+    }
+
+    // Build a SecretKeyEntry with the provided AES key or generate a random one
+    val secretKeyEntry: SecretKeyEntry = genSecretKeyEntry(aesKey?.decodeHex(), "AES")
+
+    // Add AES key to keystore
+    val aesKeyParams = KeyStore.PasswordProtection(aesKeyPassword)
+    keystore.setEntry("aes_key", secretKeyEntry, aesKeyParams)
+
+    // Save keystore to disk
+    try {
+        keystore.store(keystoreOutputStream, keystorePassword)
+    } catch (e: Exception) {
+        eprintln("Couldn't save keystore to disk$e")
+        exitProcess(1)
+    }
+
+    val cipher = encrypt(algorithm, rawData, secretKeyEntry.secretKey)
+    println(cipher)
+
+    if (debug) {
+        eprintln(decrypt(algorithm, cipher, secretKeyEntry.secretKey))
+    }
+}
