b593e10fd5b476d1abacc81d392668323942b563
Jonathan Scherrer <jonathan@scherrer.ovh>
2023-04-20 14:03:28 +0200

    upd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.io.FileInputStream
+import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
 import java.security.KeyStore
@@ -13,12 +14,12 @@ import javax.crypto.spec.SecretKeySpec
 import kotlin.system.exitProcess
 
 
-val aesKey = System.getenv("AES_KEY") ?: null;
-val aesKeyPassword: CharArray = (System.getenv("AES_KEY_PASSWORD") ?: "changeit").toCharArray();
-val keystorePath: String = System.getenv("KEYSTORE_PATH") ?: "keystore.p12";
-val keystorePassword: CharArray = (System.getenv("KEYSTORE_PASSWORD") ?: "changeit").toCharArray();
-val keystoreType: String = System.getenv("KEYSTORE_TYPE") ?: "PKCS12";
-val aesAlgorithm: String = System.getenv("AES_ALGORITHM") ?: "CBC/PKCS5Padding";
+val aesKey = System.getenv("AES_KEY") ?: null
+val aesKeyPassword: CharArray = (System.getenv("AES_KEY_PASSWORD") ?: "changeit").toCharArray()
+val keystorePath: String = System.getenv("KEYSTORE_PATH") ?: "keystore.p12"
+val keystorePassword: CharArray = (System.getenv("KEYSTORE_PASSWORD") ?: "changeit").toCharArray()
+val keystoreType: String = System.getenv("KEYSTORE_TYPE") ?: "PKCS12"
+val aesAlgorithm: String = System.getenv("AES_ALGORITHM") ?: "CBC/PKCS5Padding"
 val debug: Boolean = (System.getenv("DEBUG") ?: "false").toBoolean()
 
 fun ByteArray.toHexString() = joinToString("") { "%02x".format(it) }
@@ -39,22 +40,28 @@ fun randomSecretKey(): SecretKey {
 }
 
 fun genSecretKeyEntry(key: ByteArray? = null, algo: String = "AES"): SecretKeyEntry {
-    val secretKey: SecretKey = if (key != null) SecretKeySpec(key, algo) else randomSecretKey()
+    val secretKey: SecretKey = if (key != null) {
+        SecretKeySpec(key, algo)
+    } else {
+        eprintln("Generating random AES key")
+        randomSecretKey()
+    }
+//    val secretKey: SecretKey = if (key != null) SecretKeySpec(key, algo) else randomSecretKey()
     return SecretKeyEntry(secretKey)
 }
 
 fun genIvParameterSpec(algorithm: String): IvParameterSpec {
     val iv = ByteArray(Cipher.getInstance(algorithm).blockSize)
-    SecureRandom.getInstanceStrong().nextBytes(iv);
+    SecureRandom.getInstanceStrong().nextBytes(iv)
     return IvParameterSpec(iv)
 }
 
 fun encrypt(algorithm: String, rawData: String, key: SecretKey): String {
     val iv: IvParameterSpec = genIvParameterSpec(algorithm)
-    val cipher = Cipher.getInstance(algorithm);
-    cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-    val cipherText = cipher.doFinal(rawData.toByteArray());
-    return iv.iv.toHexString() + cipherText.toHexString();
+    val cipher = Cipher.getInstance(algorithm)
+    cipher.init(Cipher.ENCRYPT_MODE, key, iv)
+    val cipherText = cipher.doFinal(rawData.toByteArray())
+    return iv.iv.toHexString() + cipherText.toHexString()
 }
 
 fun decrypt(algorithm: String, ciphertext: String, key: SecretKey): String {
@@ -62,7 +69,7 @@ fun decrypt(algorithm: String, ciphertext: String, key: SecretKey): String {
     val ivRaw = ciphertext.slice(0 until ciphertext.length/2).decodeHex()
     val iv = IvParameterSpec(ivRaw)
     val cipherData = ciphertext.slice(ciphertext.length/2 until ciphertext.length).decodeHex()
-    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+    cipher.init(Cipher.DECRYPT_MODE, key, iv)
     val data = cipher.doFinal(cipherData)
 
     return String(data)
@@ -73,46 +80,67 @@ fun main(args: Array<String>) {
         eprintln("No data to encrypt")
         exitProcess(1)
     }
-
     val algorithm = "AES/$aesAlgorithm" // Only AES supported
     val rawData: String = args[0] // Data to encrypt
     val keystore: KeyStore = KeyStore.getInstance(keystoreType)
-    val keystoreOutputStream = FileOutputStream(keystorePath)
-    val keystoreInputStream = FileInputStream(keystorePath)
+    val aesKeyParams = KeyStore.PasswordProtection(aesKeyPassword)
+    val keystoreInputStream: FileInputStream? = try {
+        FileInputStream(keystorePath)
+    } catch (e: FileNotFoundException) {
+        eprintln("Keystore doesn't exist yet, it will be created")
+        null
+    }
 
+    // Load the keystore from disk, or create an empty one
     try {
         keystore.load(keystoreInputStream, keystorePassword)
     } catch (e: IOException) {
+        eprintln("Couldn't load keystore from disk")
         if (e.cause is UnrecoverableKeyException) {
-            eprintln("Couldn't load keystore from disk, wrong password")
-            exitProcess(1)
+            eprintln("Cause: wrong password")
         }
-        // Create an empty keystore
-        keystore.load(null, keystorePassword)
+        exitProcess(1)
     } catch (e: Exception) {
         eprintln("Couldn't load keystore from disk.")
         exitProcess(1)
+    } finally {
+        keystoreInputStream?.close()
+    }
+
+    // Use aes key from env variable if provided, or from keystore
+    val keystoreEntry: SecretKeyEntry? = keystore.getEntry("aes_key", aesKeyParams) as? SecretKeyEntry
+    val aesKeyByteArray: ByteArray? = if (keystoreEntry != null) {
+        eprintln("Using keystore entry")
+        keystoreEntry.secretKey?.encoded
+    } else if (aesKey != null) {
+        eprintln("Using provided key")
+        aesKey.decodeHex()
+    } else {
+        null
     }
 
     // Build a SecretKeyEntry with the provided AES key or generate a random one
-    val secretKeyEntry: SecretKeyEntry = genSecretKeyEntry(aesKey?.decodeHex(), "AES")
+    val secretKeyEntry: SecretKeyEntry = genSecretKeyEntry(aesKeyByteArray, "AES")
 
     // Add AES key to keystore
-    val aesKeyParams = KeyStore.PasswordProtection(aesKeyPassword)
     keystore.setEntry("aes_key", secretKeyEntry, aesKeyParams)
 
+    val keystoreOutputStream = FileOutputStream(keystorePath)
     // Save keystore to disk
     try {
         keystore.store(keystoreOutputStream, keystorePassword)
     } catch (e: Exception) {
         eprintln("Couldn't save keystore to disk$e")
         exitProcess(1)
+    } finally {
+        keystoreOutputStream.close()
     }
 
     val cipher = encrypt(algorithm, rawData, secretKeyEntry.secretKey)
     println(cipher)
 
     if (debug) {
+        eprintln(secretKeyEntry.secretKey.encoded.toHexString())
         eprintln(decrypt(algorithm, cipher, secretKeyEntry.secretKey))
     }
 }
